352d552e20cf71120fbfa0a458cfdf83553258c9
Onion4070 <naonaosub.0704@gmail.com>
2025-11-15 19:13:47 +0900

    GamePadクラスの作成

---
+++ viewer/GamePad.cpp
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include <iostream>
+#include "GamePad.h"
+using std::cout;
+using std::endl;
+
+GamePad::GamePad() : serial(io){
+	
+}
+
+GamePad::~GamePad() {
+	Disconnect();
+}
+
+void GamePad::Connect(const std::string& portName) {
+	if (connected) {
+		cout << "Already connected." << endl;
+		return;
+	}
+
+	try {
+		serial.open(portName);
+
+		serial.set_option(asio::serial_port_base::baud_rate(115200));
+		serial.set_option(asio::serial_port_base::character_size(8));
+		serial.set_option(asio::serial_port_base::parity(asio::serial_port_base::parity::none));
+		serial.set_option(asio::serial_port_base::stop_bits(asio::serial_port_base::stop_bits::one));
+		serial.set_option(asio::serial_port_base::flow_control(asio::serial_port_base::flow_control::none));
+
+		// 別スレッドで実行
+		ioThread = std::thread([this]() { 
+			io.run(); 
+		});
+
+		connected = true;
+		cout << "Connected to " << portName << endl;
+	}
+	catch (std::exception& e) {
+		cout << "Error connecting to " << portName << ": " << e.what() << endl;
+		connected = false;
+	}
+}
+
+void GamePad::Disconnect() {
+	if (!connected) {
+		return;
+	}
+	try {
+		serial.close();
+		io.stop();
+		if (ioThread.joinable()) {
+			ioThread.join();
+		}
+		connected = false;
+		cout << "Disconnected." << endl;
+	}
+	catch (std::exception& e) {
+		cout << "Error during disconnect: " << e.what() << endl;
+	}
+}
---
+++ viewer/GamePad.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <asio.hpp>
+class GamePad
+{
+public:
+	GamePad();
+	~GamePad();
+	void Connect(const std::string& portName);
+	void Disconnect();
+	bool IsConnected() const { return connected; }
+
+private:
+	asio::io_context io;
+	asio::serial_port serial;
+	std::thread ioThread;
+	bool connected = false;
+};
+
